ad3768d9c769fc7d118f355fa5d1fb172870777b
GalacticPast <192919385+GalacticPast@users.noreply.github.com>
2026-05-14 15:52:40 -0400

    fix

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -8,7 +8,7 @@ uniform sampler2D u_main;
 
 out vec4 fragColor;
 
-float circle(vec2 uv, vec2 pos, float radius,float blur)
+float Circle(vec2 uv, vec2 pos, float radius,float blur)
 {
     uv -= pos;
     float dist = length(uv); // magnitude of how far it is from the origin 
@@ -17,36 +17,52 @@ float circle(vec2 uv, vec2 pos, float radius,float blur)
     return c_step;
 }
 
-float smiley(vec2 uv, vec2 pos, vec2 size)
+float Smiley(vec2 uv, vec2 pos, vec2 size)
 {
     uv -= pos; 
     uv *= size;
 
-    float face = circle(uv, vec2(0,0), 0.2, 0.01);
-    float l_eye = circle(uv,vec2(-0.1, 0.05), 0.05,0.01);
-    float r_eye = circle(uv,vec2(0.1,0.05), 0.05,0.01);
-    float mouth_u = circle(uv, vec2(0,-0.075), 0.08,0.01);
-    float mouth_l = circle(uv, vec2(0,-0.065), 0.08,0.01);
+    float face = Circle(uv, vec2(0,0), 0.2, 0.01);
+    float l_eye = Circle(uv,vec2(-0.1, 0.05), 0.05,0.01);
+    float r_eye = Circle(uv,vec2(0.1,0.05), 0.05,0.01);
+    float mouth_u = Circle(uv, vec2(0,-0.075), 0.08,0.01);
+    float mouth_l = Circle(uv, vec2(0,-0.065), 0.08,0.01);
      
     float mask = face - l_eye - r_eye - (mouth_u - mouth_l);
 
     return mask;
 }
 
+float Band(float t, float start, float end, float blur)
+{
+    float band_a = smoothstep(start - blur, start + blur, t);
+    float band_b = smoothstep(end - blur,end + blur, t);
+    return band_a - band_b;
+}
+
+float Rect(vec2 uv, float left, float right, float top, float bottom, float blur)
+{
+    float band_1 = Band(uv.x, left, right, blur);
+    float band_2 = Band(uv.y, bottom, top, blur);
+
+    return band_1 * band_2;
+}
+
 void main() 
 {
-    float aspect_ratio = u_resolution.x / u_resolution.y;
 
     vec2 frag_coord = vec2(gl_FragCoord.x, gl_FragCoord.y);
+    float aspect_ratio = u_resolution.x / u_resolution.y;
 
     vec2 uv = frag_coord / u_resolution;  
     uv -= 0.5; 
     uv.x *= aspect_ratio; // 1 unit of X == 1 Unit of y
     
-    //float smile = smiley(uv, vec2(0, 0), vec2(2));
-    
-    //vec3 color = vec3(1,0,1) * smile;
+    uv.x *= sin(u_time);
+    float rect = Rect(uv, -0.3, 0.3, 0.1, -0.1, 0.005);
 
-    fragColor = vec4(vec3(1), 1.0);
+    vec3 color = vec3(1,1,1) * rect;
+
+    fragColor = vec4(color, 1.0);
 }
 
