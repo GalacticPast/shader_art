f4232a1d0f614d2009e57195f736a3ba622bd219
GalacticPast <192919385+GalacticPast@users.noreply.github.com>
2026-06-09 23:08:52 -0400

    fix

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -6,51 +6,76 @@ uniform float u_time;         // seconds since playback started
 
 uniform sampler2D u_main;
 
-out vec4 frag_color;
+out vec4 fragColor;
 
-#define PI 3.1459
-#define MAX_DIST 16 
-#define MIN_DIST 0.001 
+#define PI 3.14159
 
-float Sd_sph(vec3 ray_pos, vec3 sph_pos, float radius)
+float Sd_sphere(vec3 p, vec3 sph_pos, float radius)
 {
-    return length(ray_pos - sph_pos) - radius;
+    float sph = length(p - sph_pos) - radius;
+    return sph;
 }
 
-mat2 Rot2d(float rad)
+float N21(vec2 p)
 {
-    float a = cos(rad);
-    float b = sin(rad);
-    return mat2(a, -b, b, a);
+    float rand = fract(sin(p.x * 191.123 + p.y * 234.342) * 8324.84353); 
+    return rand;
+}
+
+mat2 Rot2D(float angle){
+    float a = cos(angle);
+    float b = sin(angle);
+    return mat2(a, -b,
+                b,  a);
+}
+
+float Value_noise(vec2 uv)
+{
+    vec2 id = floor(uv);
+    vec2 f = fract(uv); 
+    
+    f = smoothstep(0.0, 1.0, f);
+
+    float l_b_hash = N21(id);
+    float r_b_hash = N21(id + vec2(1.0, 0.0)); 
+    float l_t_hash = N21(id + vec2(0.0, 1.0)); 
+    float r_t_hash = N21(id + vec2(1.0, 1.0)); 
+    
+    float lr_b = l_b_hash + (r_b_hash - l_b_hash) * f.x;
+    float lr_t = l_t_hash + (r_t_hash - l_t_hash) * f.x;
+    float lr_bt = lr_b + (lr_t - lr_b) * f.y;
+
+    return lr_bt; 
+}
+
+float Fbm(vec2 uv){
+    float fbm = 0.0; 
+    float amp = 1.0;
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        fbm += Value_noise(uv) * amp;
+        uv *= 3.0; // freq 
+        amp *= 0.5;
+    }
+    return fbm / 2; 
 }
 
 float Map(vec3 p)
 {
-    float plane_bottom = p.y;
-    float plane_top    = 6 - p.y;
-    
-    return  min(plane_bottom, plane_top);
+    float plane = p.y + 1;
+    float noise = Fbm(p.xz + 0.05 * u_time);
+    float radius = 2.5 +  noise * 0.5;
+    //float radius = 2.5;
+    float sph = Sd_sphere(p, vec3(0.0, 0.0, 0.0), radius);
+    //float d = min(sph, plane);
+    float d = sph;
+    return d;
 }
-
-float Render(vec3 r_o, vec3 r_d)
-{
-    float t = 0.0;
-    
-    for(int i = 0 ; i < MAX_DIST ; i++)
-    {
-        vec3 p = r_o + r_d * t;
-        float d = Map(p);
-        t += d;
-        if(d < MIN_DIST || d > MAX_DIST)break;
-    }
-    return t;
-}
-
-vec3 Get_normal(vec3 p)
+vec3 Get_normal(vec3 p) 
 {
     vec2 e = vec2(0.001, 0.0); // A tiny offset
-
-    // partial derivatives. Gradients see multivariable calculus for this  
+    
+    // Sample the scene slightly to the left/right on each axis
     float dx = Map(p + e.xyy) - Map(p - e.xyy);
     float dy = Map(p + e.yxy) - Map(p - e.yxy);
     float dz = Map(p + e.yyx) - Map(p - e.yyx);
@@ -58,102 +83,73 @@ vec3 Get_normal(vec3 p)
     return normalize(vec3(dx, dy, dz));
 }
 
-float Get_AO(vec3 p, vec3 n) 
+float Get_light(vec3 p)
 {
-    float occ = 0.0;
-    float weight = 1.0;
-    for(int i = 0; i < 5; i++) {
-        float len = 0.01 + 0.05 * float(i);
-        float dist = Map(p + n * len);
-        
-        // If the distance to the nearest object is less than how far 
-        // we stepped, we are inside a crevice.
-        occ += (len - dist) * weight;
-        weight *= 0.85; // Less impact the further away we step
-    }
-    return 1.0 - clamp(0.5 * occ, 0.0, 1.0);
+    vec3 light_pos = vec3(0, 0, -10);
+    vec3 N = Get_normal(p);
+
+    vec3 light_dir = normalize(light_pos - p);
+
+    float light = dot(N, light_dir);
+    return max(light, 0.0);
 }
 
-float Get_shadow(vec3 p, vec3 light_dir)
-{
-    float t = 0.05; 
-    
-    float max_dist = 20.0; 
-
-    for(int i = 0; i < 50; i++) {
-        float d = Map(p + light_dir * t); 
-        
-        if(d < 0.001) {
-            return 0.1; 
-        }
-        
-        t += d;
-        if(t > max_dist) break;
-    }
-    
-    return 1.0; 
-}
-
-float Get_light(vec3 pos, vec3 r_o)
-{
-    vec3 light_pos = vec3(0.0, 20.0, 0.0);
-
-    vec3 N = Get_normal(pos); 
-    vec3 light_dir = normalize(light_pos - pos); 
-    float diffuse = max(dot(light_dir, N), 0.0);
-
-    vec3 camera_dir = normalize(r_o - pos);
-    vec3 perfect_spec = reflect(-light_dir, N); // phill model
-
-    vec3 half_vec = normalize(light_dir + camera_dir); // blinn model
-    float spec_amount = max(dot(camera_dir, half_vec), 0.0);
-    float specular = pow(spec_amount, 100.0); 
-
-    //4 + 4*smoothstep(0,0.7,sin(x+t))
-    vec2 falloff = fract(pos.xz / 3); 
-    falloff -= 0.5;
-    float light = 0.0;
-    light = 0.05 / length(falloff);
-    //float light = diffuse;
-    float shadow = Get_shadow(pos, light_dir);
-    float ao = Get_AO(pos, N);
-
-    // light *= shadow;
-    // light *= ao;
-
-    return light; 
-}
 
 void main()
 {
-    vec2 res = u_resolution.xy;
-    vec2 frag_coord = vec2(gl_FragCoord.x, gl_FragCoord.y);
-    vec2 uv = (frag_coord * 2.0)/ u_resolution;  
-    uv -= 1.0;  
-    uv.x *= u_resolution.x / u_resolution.y; 
+    vec2 uv = (gl_FragCoord.xy * 2)/ u_resolution.xy;
+    uv -= 1; 
+    uv.x *= u_resolution.x / u_resolution.y;
+    //zoomed in
+    vec3 r_o = vec3(0,1.8,-2.3);
+    vec3 r_d = normalize(vec3(vec2(uv), 1.0));
 
-    vec3 r_o = vec3(0, 2, -5);
-    vec3 r_d = normalize(vec3(uv, 1.0)); 
+    r_o.xz *= Rot2D(PI);
+    r_d.xz *= Rot2D(PI);
+    //vec3 r_o = vec3(0,1.0,-3);
+    //vec3 r_o = vec3(0.0,0.0,-5);
 
-    float rad = PI / 6;
-    mat2 rot = Rot2d(rad);
-    // r_o.yz *= rot; 
-    // r_d.yz *= rot; 
+    float t = 0.0;
+    float glow = 0.0;
 
-    float hit_dist = Render(r_o, r_d);
-    vec3 final_color = vec3(0.0);
-
-    // 1. Surface Lighting
-    if(hit_dist < MAX_DIST) 
+    bool hit = false;
+    for(int i = 0 ; i < 100 ; i++)
     {
-        vec3 p = r_o + r_d * hit_dist;
-
-        float surface_light = Get_light(p, r_o); 
-        final_color = vec3(abs(sin(p.x * 0.2)), abs(sin(p.y * 0.3)), abs(sin(p.z * 0.2))) * surface_light; 
-        vec2 gv = fract(p.xz);
-        //float grid_line = smoothstep(0.92, 0.97, max(gv.x, gv.y));
-        //final_color += vec3(1) * grid_line;
+        vec3 pos = r_o + r_d * t;
+        float d = Map(pos);
+        t += d;
+        if(d < 0.001)
+        {
+            hit = true;
+            break;
+        } 
+        if(d > 100.0)break;
     }
 
-    frag_color = vec4(final_color, 1.0); 
+    vec3 hit_pos = r_o + r_d * t;
+    float light =  Get_light(hit_pos);
+    vec3 color = vec3(0.0);
+        
+    if(hit)
+    {
+        vec3 normal = normalize(hit_pos);
+
+        float u = atan(normal.z, normal.x); 
+        float v = asin(normal.y);           
+
+        float densityU = 10.0;
+        float densityV = 10.0;
+        
+        vec2 g = fract(vec2(u * densityU, v * densityV));
+        color += 0.05 / length(g) * vec3(1.0);
+        
+        // float line_thickness = 0.95; 
+        // if(g.x > line_thickness || g.y > line_thickness)
+        // {
+        //     color = vec3(0.0); 
+        // }
+
+    }
+
+    fragColor = vec4(color, 1.0);
 }
